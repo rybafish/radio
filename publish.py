b5212d1d02e735bbb73f0c4917b8753acc2e736c
rybafish <rybafishcharts@gmail.com>
2025-12-24 16:47:00 +0100

    minor uptd

--- publish.py
+++ publish.py
@@ -1,6 +1,6 @@
 import boto3
 from botocore.exceptions import ClientError
-from utils import cfg
+from utils import cfg, log
 import utils
 import os
 from generate import generate
@@ -21,7 +21,7 @@ def s3Connect():
     s3 = boto3.resource('s3', aws_access_key_id=aws_key, aws_secret_access_key=aws_secret)
     s3client = boto3.client('s3', aws_access_key_id=aws_key, aws_secret_access_key=aws_secret)
 
-    print('connected...')
+    log('connected...')
     return s3client
 
 def cleanup(s3client):
@@ -39,29 +39,29 @@ def publish(s3client):
     feedFileName = subfolder + feedFile
 
     try:
-        print(f'Uploading feed itself: {feedFileName}...')
+        log(f'Uploading feed itself: {feedFileName}...')
         s3client.upload_file('podcast.xml', bucket, feedFileName, ExtraArgs={'ACL': 'public-read', 'ContentType': 'application/xml'})
 
         if cfg('uploadContent'):
             files = checkUploads()
 
         if not files:
-            print('No content to upload.')
+            log('No content to upload.')
 
         for f in files:
             trgt = f'{subfolder}{f}'
             src = os.path.join(storage, f)
-            print(f'Uploading {src} --> {trgt}...', end='', flush=True)
+            log(f'Uploading {src} --> {trgt}...', nots=True)
             s3client.upload_file(src, bucket, trgt, ExtraArgs={'ACL': 'public-read', 'ContentType': 'application/xml'})
 
             os.remove(src)
-            print(' [D]')
+            log(' [D]')
 
     except ClientError as e:
-        print('\n[E] '+str(e))
+        log('\n[E] '+str(e))
         return False
 
-    print('done')
+    log('publish done')
 
     return True
 

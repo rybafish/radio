2cb3c2088dc4287b4d06f7ced1c1f7a0897333fb
rybafish <rybafishcharts@gmail.com>
2026-09-16 22:52:27 +0200

    adjust layout for mobiles

--- radio.py
+++ radio.py
@@ -47,6 +47,7 @@ HTML_FORM = """
 <html>
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Radio Nyanya</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>

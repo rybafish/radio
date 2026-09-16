7cc57be2c7d77cd90bfe81da5c14c9fe7971e5fc
rybafish <rybafishcharts@gmail.com>
2026-09-17 00:43:31 +0200

    change shade

--- radio.py
+++ radio.py
@@ -60,7 +60,7 @@ head_html = '''
             width: 250px;
             font-size: 18px;
             padding: 10px;
-            border: 1px solid #ddd;
+            border: 1px solid #bbb;
         }
 
         button {
